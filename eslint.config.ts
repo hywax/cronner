1d2f75825a214ed01a6eac40ae35cf195af446f6
hywax <me@hywax.space>
2024-01-29 17:40:25 +0500

    chore: code linting

---
+++ eslint.config.ts
@@ -0,0 +1,28 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    typescript: true,
+    ignores: [
+      '.github',
+      '.output',
+      'dist',
+      'node_modules',
+    ],
+    vue: {
+      overrides: {
+        'vue/block-order': ['error', {
+          order: ['template', 'script', 'style'],
+        }],
+      },
+    },
+  },
+  {
+    rules: {
+      'style/brace-style': ['error', '1tbs'],
+      'style/arrow-parens': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'antfu/consistent-list-newline': 'off',
+    },
+  },
+)
